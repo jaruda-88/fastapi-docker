2fbcc76eaad0198369f5dfeb2163b60c3bf44156
jaruda-88 <jaruda88@gmail.com>
2024-03-12 14:52:44 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -18,3 +19,6 @@ def create_app():
 
 
 app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
