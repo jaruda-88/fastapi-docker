2daea14658e7340da23f2109d07c357d91f2c75f
jaruda-88 <jaruda88@gmail.com>
2024-03-12 17:59:49 +0900

    update

--- app/databases/conn.py
+++ app/databases/conn.py
@@ -1,3 +1,63 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-# DB_URL = 'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
+class DBConnector:
+    
+    def __init__(self, app:FastAPI=None, **kwargs) -> None:
+        
+        self._engine = None
+        self._session = None
+
+        if app is not None:
+            self.initialize(app=app, **kwargs)
+
+    def initialize(self, app:FastAPI, **kwargs) -> None:
+
+        db_url = 'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
+        pool_recycle = 900
+        echo = True
+
+        self._engine = create_engine(
+            db_url,
+            echo=echo,
+            pool_recycle=pool_recycle,
+            pool_pre_ping=True
+        )
+        self._session = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
+
+        @app.on_event("startup")
+        def startup():
+            self._engine.connect()
+
+
+        @app.on_event("shutdown")
+        def shutdown():
+            self._session.close_all()
+            self._engine.dispose()
+
+    def get_db(self):
+        
+        if self._session in None:
+            raise Exception("colled initialize")
+        
+        db_session = None
+
+        try:
+            db_session = self._session()
+            yield db_session
+        finally:
+            db_session.close()
+
+    @property
+    def session(self):
+        return self.get_db
+    
+    @property
+    def engine(self):
+        return self._engine
+    
+db_conn = DBConnector
+Base = declarative_base()
